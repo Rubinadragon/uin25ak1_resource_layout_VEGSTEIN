794c88b5fc575ee80ea3212878c7619944b6325d
Rubinadragon <cecilive@hiof.no>
2025-01-16 19:24:56 +0100

    Printe overskrifter og tekst, ikke liste ennå
    todo:
    -printe ut <li> basert på hvor mange objekter det er i laste
    -Endre farge på fane når den trykkes på
    -Skjule tekst ikke assosiert med fanetekst
    -Muligens noe mer jeg ikke kommer på

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 //https://stackoverflow.com/questions/10510063/javascript-object-reference-linked-to-object-in-array
 //https://www.w3schools.com/howto/howto_js_active_element.asp
 //https://stackoverflow.com/questions/72348956/how-to-map-through-a-deeply-nested-array-of-objects
+//https://stackoverflow.com/questions/32141291/javascript-reflection-get-nested-objects-path
 //https://stackoverflow.com/questions/34398279/map-and-filter-an-array-at-the-same-time
 //https://stackoverflow.com/questions/48707227/how-to-filter-a-javascript-map
+//https://stackoverflow.com/questions/54723528/how-to-create-tabs-in-javascript
 
 const html = document.getElementById("HTMLtab")
 const css = document.getElementById("CSStab")
@@ -13,66 +15,64 @@ const sanity = document.getElementById("sanitytab")
 const header = document.getElementById("tabs")
 const btn = header.getElementsByClassName("tab")
 
-html.addEventListener("click", switchTab)
+//console.log(btn)
+
+/*html.addEventListener("click", switchTab)
 css.addEventListener("click", switchTab)
 js.addEventListener("click", switchTab)
 react.addEventListener("click", switchTab)
-sanity.addEventListener("click", switchTab)
+sanity.addEventListener("click", switchTab)*/
 
 //Headerprint
 headerHTML = []
 
-//Pushe fra ressurser inn i resourceHTML
+//Pushe headerelementer fra ressurser inn i resourceHTML
 resources.forEach((object, index) => {
     headerHTML.push(object.category)
 })
 let resourceHTML = ""
 
-//Startfane
+//lagre <li>
+let listElement = ""
+//console.log(sources)
+//Generere dynamisk mengde <li>
+/*sources.map((source, index) => listElement +=
+`<li>
+    <a href="#">${resources[index].sources[index]}</a>
+</li>`)*/
 
+
+//Startfane
 resources.map((resource, index) => resourceHTML +=
-        `<h2>${resources[index].category}</h2>
-            <p>${resources[index].text}</p>
-            <ul>
-                <li>
-                    <a href="#"></a>
-                </li>
-                <li>
-                    <a href="#"></a>
-                </li>
-                <li>
-                    <a href="#"></a>
-                </li>
-            </ul>`
-    )
-    console.log(resourceHTML)
+`<h2>${resources[index].category}</h2>
+    <p>${resources[index].text}</p>
+    <ul>
+        ${listElement}
+    </ul>`
+)
+console.log(listElement)
+
+    //console.log(resourceHTML)
 
 //Bytte faner
+for(let teller = 0; teller < btn.length; teller++){
+    btn[teller].addEventListener("click", function(){
+            resourceHTML = ""
+            //Filtrere info
+            
 
-
-//Filtrere info
-
-
-function switchTab(){
-    resourceHTML = ""
-    resources.map((resource, index) => resourceHTML +=
-        `<h2>${resources[index].category}</h2>
-            <p>${resources[index].text}</p>
-            <ul>
-                <li>
-                    <a href="#"></a>
-                </li>
-                <li>
-                    <a href="#"></a>
-                </li>
-                <li>
-                    <a href="#"></a>
-                </li>
-            </ul>`
-    )
-    console.log(resourceHTML)
-    document.getElementById("article").innerHTML = resourceHTML
+            resources.map((resource, index) => resourceHTML +=
+            `<h2>${resources[index].category}</h2>
+                <p>${resources[index].text}</p>
+                <ul>
+                </ul>`
+        )
+        console.log(btn[teller])
+        document.getElementById("article").innerHTML = resourceHTML
+    })
 }
+
+
 
 document.getElementById("HTMLtab").innerHTML = headerHTML[0]
 document.getElementById("CSStab").innerHTML = headerHTML[1]
