6630c4bd1821a5a67df98babb810a17a1f0da753
Rubinadragon <cecilive@hiof.no>
2025-01-29 18:34:21 +0100

    Rydding i kommentarer

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ let listElement = ""
 //Alle instanser av map og filter-kombo basert på første forslag fra denne siden: 
 //https://stackoverflow.com/questions/70894999/using-map-and-filter-at-the-same-time-in-javascript
 /*I tillegg gikk noe hjelp fra chatGPT med syntaksen, det jeg endte med å bruke derfra var å flytte source inni map og legge til krøllparenteser etter arrowfunksjonen til map. 
-Lenken til chatten er en ny chat, men opprinnelig ble det skrevet inn i en eldre chat fra tidligere semestere,
-så da kopierte jeg inn det jeg hadde skrevet inn i den forrige chatten i den nye, for at det jeg skrev i andre oppgaver ikke skulle bli blandet med det jeg skrev i denne.
+Lenken til chatten er en ny chat, men opprinnelig ble det skrevet inn i en eldre chat fra tidligere semestere.
+Så da kopierte jeg inn det jeg hadde skrevet inn i den forrige chatten i den nye, for at det jeg skrev i andre oppgaver ikke skulle bli blandet med det jeg skrev i denne.
 Lenke til chatten her: https://chatgpt.com/share/679a61c0-d324-8006-87ad-2e89a2878115
 // Den gamle chatten er her, men jeg har ikke tatt i bruk noe annet enn det jeg nevnte tidligere, selv om jeg også har spurt den om flere ting senere:
-//  https://chatgpt.com/c/6707a71d-adb0-8006-8e83-aa4342ab7df7*/
+// https://chatgpt.com/c/6707a71d-adb0-8006-8e83-aa4342ab7df7*/
 resources.filter((source, index) => index === 0).map((resource) => 
     resource.sources.map((source) => {listElement += 
     `<li>
